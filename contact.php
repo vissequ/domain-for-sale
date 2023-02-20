d92e2a4212310981ea3a94c49b998f134f949964
vissequ <93226346+vissequ@users.noreply.github.com>
2023-02-20 08:14:30 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@
 	if (isset($_POST['email'])) {
 
 		// EDIT THE 2 LINES BELOW AS REQUIRED
-		$email_to = "hey@sarozpoddar.com.np";
-		$email_subject = "My offer for [Your Domain]";
+		$email_to = "connor.fabiano@gmail.com";
+		$email_subject = "My offer for Celo.art";
 
 
 		$name = $_POST['name']; // required
